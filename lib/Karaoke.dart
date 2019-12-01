e2d1a195767d5debe0e5f053a9bb7accabc04d71
shgini27 <go_shuhrat@yahoo.com>
2019-12-01 12:57:46 +0500

    video url changed

--- lib/Karaoke.dart
+++ lib/Karaoke.dart
@@ -1,5 +1,5 @@
 class Karaoke {
   static const String APP_TITLE = "Vplayer";
   static const String MAIN_PAGE = "Karaoke Page";
-  static const String FULL_VIDEO_URL = "https://voicetm.com/img/videos/db1d3c62-7b07-44f0-bf12-41e395c6ad42.SampleVideo_1280x720_2mb.mp4";
+  static const String FULL_VIDEO_URL = "http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4";
 }
